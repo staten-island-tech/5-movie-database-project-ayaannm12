d89d21a3cbceda20a9210cc6438b5a391f979105
ayaannm12 <ayaanm5352@gmail.com>
2025-11-17 12:19:49 -0500

    aaa
    aaa

--- app5.py
+++ app5.py
@@ -4,8 +4,7 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
-want1 = input("what movie do you want to search for?")
-for movie in data:
-    if movie["title"] == want1:
+want1 = input("what movie do you want to search for?").lower().strip()
+if movie["title"] == want1:
         print(movie["title"]) 
         print(movie["year"])
